56d2635a4c6eef4d8d442aa1e5d1559b09a5d083
ndt2804 <nduytan.uit.a3@gmail.com>
2024-12-24 13:54:38 +0700

    feat: Use helmet and rate limit to protect the application

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import express from 'express'
 import http from 'http';
 import cors from 'cors';
 import route from './routes/route.js';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 const app = express()
 const server = http.createServer(app);
 app.use(express.json());
+app.use(helmet());
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 50,
+    message: 'Too many requests from this IP, please try again later.',
+});
+app.use(limiter);
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     methods: ['GET', 'POST'],
