4443d5241088afacbc157557320b56b65a26fde7
ndt2804 <nduytan.uit.a3@gmail.com>
2024-12-24 13:30:11 +0700

    fix: add url cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express()
 const server = http.createServer(app);
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
 }));
